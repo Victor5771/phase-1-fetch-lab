263a6157e991db5c74035ca2b16cf9ae70b5ab06
Victor <victor.kyalo@student.moringaschool.com>
2023-10-03 14:30:41 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => {
+      renderBooks(data);
+      return data; // Return the JSON data for testing purposes
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
 }
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
